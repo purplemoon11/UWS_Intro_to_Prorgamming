9d4059624770837f51f500f0802a7c31c98a64fa
Abishkar Karki <11abishkar.karki11@gmail.com>
2026-05-31 13:19:52 +0100

    new: Lab Work - Lab 3

---
+++ lab3.py
@@ -0,0 +1,47 @@
+# Question 1
+# Create a fully commented program to:
+# prompt for and read in a name and output “He’s not the Messiah, he’s a very naughty boy” if the name
+# equals “Brian”, and output “Name not recognised” otherwise.
+    # name = input("What is your name? ")
+    # if name == "Brian":
+    #     print(f"He’s not the Messiah, he’s a very naughty boy")
+    # else:
+    #     print(f"Name not recognised")
+
+# Question 2
+# Create a fully commented program to:
+# prompt for and read in a whole number and output “Too low” if it’s less than 50, “Too high” if it’s
+# greater than 50 and “correct” if it’s equal to 50.
+    #Ask the user for the number
+    # number = int(input("Enter a number: "))
+    # Check the number and print the result
+    # if number < 50:
+    #     print("Too Low")
+    # elif number > 50:
+    #     print("Too High")
+    # else:
+    #     print("Correct")
+
+# Question 3 (This is a wee bit trickier)
+# Create a fully commented program to:
+# calculate the cost of UWS Gym membership based on the following information:
+# under 18 £24
+# over 18 £44
+# UWS student Free
+    # Ask the user for their age
+    # age = int(input("Enter your age: "))
+    # Ask if the user is a UWS student (yes/no)
+    # student = input("Are you a UWS student? (yes/no): ")
+    # Check if the user is a UWS student first
+    # if student == "yes":
+    #     cost = 0
+    #     print("You are a UWS student. Membership is FREE.")
+    # If not a student, check age
+    # else:
+    #     if age < 18:
+    #         cost = 24
+    #         print("Under 18 membership cost is £24.")
+    #     else:
+    #         cost = 44
+    #         print("Over 18 membership cost is £44.")
+    # print(f"Total cost: {cost} £")
